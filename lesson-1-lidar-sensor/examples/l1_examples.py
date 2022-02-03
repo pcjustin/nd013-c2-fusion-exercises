a555c2523c37b02f6898fdd773d3acb7842ffe94
Justin Lu <pcjustin@icloud.com>
2022-02-03 11:10:37 +0800

    feat: add C1-3-3 Find out the number of individual laser LEDs of the top LiDAR sensor

--- lesson-1-lidar-sensor/examples/l1_examples.py
+++ lesson-1-lidar-sensor/examples/l1_examples.py
@@ -155,6 +155,7 @@ def print_vfov_lidar(frame, lidar_name):
     # compute and print vfov in degrees
     print(vfov_rad*180/np.pi)
 
+    print(len(calib_lidar.beam_inclinations))
 
 # Example C1-3-2 : display camera image
 def display_image(frame):
