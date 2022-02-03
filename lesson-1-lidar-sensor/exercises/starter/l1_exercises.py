89a167bb08648291b062288686a2f931d664dcf0
Justin Lu <pcjustin@icloud.com>
2022-02-03 14:46:49 +0800

    feat: add C1-5-2

--- lesson-1-lidar-sensor/exercises/starter/l1_exercises.py
+++ lesson-1-lidar-sensor/exercises/starter/l1_exercises.py
@@ -42,10 +42,23 @@ def print_pitch_resolution(frame, lidar_name):
 
     print("Exercise C1-5-2")
     # load range image
-        
-    # compute vertical field-of-view from lidar calibration 
+    lidar = [obj for obj in frame.lasers if obj.name == lidar_name][0] # get laser data structure from frame
+    ri = []
+    if len(lidar.ri_return1.range_image_compressed) > 0: # use first response
+        ri = dataset_pb2.MatrixFloat()
+        ri.ParseFromString(zlib.decompress(lidar.ri_return1.range_image_compressed))
+        ri = np.array(ri.data).reshape(ri.shape.dims)
+   
+    # compute vertical field-of-view from lidar calibration
+    lidar_calib = [obj for obj in frame.context.laser_calibrations if obj.name == lidar_name][0] # get laser calibration
+    min_pitch = lidar_calib.beam_inclination_min
+    max_pitch = lidar_calib.beam_inclination_max
+    vfov = max_pitch - min_pitch
 
     # compute pitch resolution and convert it to angular minutes
+    pitch_res_rad = vfov / ri.shape[0]
+    pitch_res_deg = pitch_res_rad * 180 / np.pi
+    print("pitch angle resolution = " + '{0:.2f}'.format(pitch_res_deg) + "°")
 
 
 # Exercise C1-3-1 : print no. of vehicles
