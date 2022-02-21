1906196992bf9d60954d5478dcf405b80e08e3a4
Justin Lu <pcjustin@icloud.com>
2022-02-21 09:16:38 +0800

    feat: add C2-4-6

--- lesson-2-object-detection/exercises/starter/l2_exercises.py
+++ lesson-2-object-detection/exercises/starter/l2_exercises.py
@@ -22,16 +22,21 @@ import numpy as np
 import zlib
 
 import matplotlib
+import matplotlib.pyplot as plt
 matplotlib.use('wxagg') # change backend so that figure maximizing works on Mac as well     
 import matplotlib.pyplot as plt
 
 # Exercise C2-4-6 : Plotting the precision-recall curve
-# def plot_precision_recall():
+def plot_precision_recall():
 
     # Please note: this function assumes that you have pre-computed the precions/recall value pairs from the test sequence
     #              by subsequently setting the variable configs.conf_thresh to the values 0.1 ... 0.9 and noted down the results.
     
-    # Please create a 2d scatter plot of all precision/recall pairs 
+    # Please create a 2d scatter plot of all precision/recall pairs
+    P = [0.97, 0.94, 0.93, 0.92, 0.915, 0.91, 0.89, 0.87, 0.82]
+    R = [0.738, 0.738, 0.743, 0.746, 0.746, 0.747, 0.748, 0.752, 0.754]
+    plt.scatter(R, P)
+    plt.show()
 
 
 
